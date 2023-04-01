e4d0ab4c15507f18482fbc1e2c70359c97427040
Mohammdreza.TH <mth.cb1446@gmail.com>
2023-04-02 01:06:06 +0430

    fixing deleteHandler bog

--- index.js
+++ index.js
@@ -1,25 +1,28 @@
 const title = document.getElementById("card__title");
 const describtion = document.getElementById("card__describtion");
-const submit = document.getElementById("submit");
+const save = document.getElementById("save");
 const editCard = document.querySelector(".edit");
-const deleteCard = document.querySelectorAll(".delete");
 const cardsContainer = document.getElementById("cards__container");
 
-submit.addEventListener("click", () => {
+const deleteHandler = () => {
+  let deleteCard = document.querySelectorAll(".delete");
+  deleteCard.forEach((el) => {
+    el.addEventListener("click", (event) => {
+      event.target.parentElement.remove();
+    });
+  });
+};
+
+const updateDOMWithCards = () => {
   cardsContainer.innerHTML += `<div class="card">
     <div class="card__title">${title.value}</div>
     <div class="card__describtion">
     ${describtion.value}
     </div>
     <button class="edit">edit</button>
-    <button class="delete">delete</button>
+    <button class="delete" >delete</button>
     </div>`;
-});
+    deleteHandler()
+};
 
-  deleteCard.forEach((el) => {
-    el.addEventListener("click", (event) => {
-      console.log("btn");
-      event.target.parentElement.remove();
-    });
-  });
-
+save.addEventListener("click", updateDOMWithCards);
