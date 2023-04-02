78026e695f9151d323a4ecacdd3485f59f2b4e12
Mohammdreza.TH <mth.cb1446@gmail.com>
2023-04-02 05:13:35 +0430

    adding editOrSaveHandler, and some other css stylings

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
+//Variable declaration:
 const title = document.getElementById("card__title");
 const describtion = document.getElementById("card__describtion");
-const save = document.getElementById("save");
+const add = document.getElementById("add");
 const editCard = document.querySelector(".edit");
 const cardsContainer = document.getElementById("cards__container");
-
+document.getElementsByTagName;
+//Function declaration:
 const deleteHandler = () => {
   let deleteCard = document.querySelectorAll(".delete");
   deleteCard.forEach((el) => {
@@ -12,17 +14,45 @@ const deleteHandler = () => {
     });
   });
 };
+//========================================================
+const editOrSaveHandler = () => {
+  let buttons = document.querySelectorAll(".edit, .save");
+  buttons.forEach((b) => {
+      b.addEventListener("click", (e) => {
+          let eventInputs =
+          e.target.parentElement.children[0].querySelectorAll("input");
+          //edit buttons handler
+          if (e.target.classList.contains("edit")) {
+              e.target.innerText = "save";
+              e.target.className = "save";
+              eventInputs.forEach((i) => {i.disabled =false})
+              //save buttons handler
+    } else if (e.target.classList.contains("save")) {
+        e.target.innerText = "edit";
+        e.target.className = "edit";
+        eventInputs.forEach((i) => {i.disabled =true})
+      }
+    });
+  });
+};
+//========================================================
 
 const updateDOMWithCards = () => {
-  cardsContainer.innerHTML += `<div class="card">
-    <div class="card__title">${title.value}</div>
-    <div class="card__describtion">
-    ${describtion.value}
-    </div>
-    <button class="edit">edit</button>
-    <button class="delete" >delete</button>
+  if (title.value === "" && describtion.value === "") {
+    alert("please fill input boxes!");
+  } else {
+    cardsContainer.innerHTML += `
+    <div class="card">
+        <div class="card-text__container">
+            <input class="card__title" type="text" value="${title.value}" disabled/>
+            <input type="text" class="card__describtion" value="${describtion.value}" disabled />            
+        </div >
+        <button class="edit">edit</button>
+        <button class="delete" >delete</button>
     </div>`;
-    deleteHandler()
+    deleteHandler();
+    editOrSaveHandler()  }
 };
+//========================================================
 
-save.addEventListener("click", updateDOMWithCards);
+add.addEventListener("click", updateDOMWithCards);
